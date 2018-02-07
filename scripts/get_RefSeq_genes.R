0977ba59f9aac8eff52cf5f084b96f5fffb0e503
Maurits Evers <maurits.evers@anu.edu.au>
2018-02-07 13:05:17 +1100

    get_RefSeq_genes.R script now also considers ncRNA

--- scripts/get_RefSeq_genes.R
+++ scripts/get_RefSeq_genes.R
@@ -10,10 +10,13 @@ chr <- c(1:19, "X", "Y", "MT");
 
 # Get all genes
 genes <- getBM(
-    attributes = c("refseq_mrna", "ensembl_gene_id", "external_gene_name", "entrezgene"),
+    attributes = c("refseq_mrna", "refseq_ncrna", "ensembl_gene_id", "external_gene_name", "entrezgene"),
     filters = "chromosome_name",
     values = chr,
     mart = mart);
+genes <- cbind(refseq_gene_id = "", genes);
+genes$refseq_gene_id <- ifelse(nchar(genes$refseq_mrna) > 0, genes$refseq_mrna, genes$refseq_ncrna);
+
 
 # Save genes
-write.csv(genes[order(genes$refseq_mrna, genes$ensembl_gene_id), ], "refseq_gene_IDs.csv", row.names = FALSE);
+write.csv(genes[order(genes$refseq_mrna, genes$ensembl_gene_id), -c(2, 3)], "refseq_gene_IDs.csv", row.names = FALSE);
